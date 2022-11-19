2148b9574513c964c741e2d4cdcb918db63105c1
Michael-Chacon <michaelchacon02@gmail.com>
2022-11-19 12:02:56 -0500

    Login

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title')
+    Registrate en DevStagram
+@endsection
+
+@section('contenido')
+    
+@endsection
---
+++ resources/views/principal.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('title')
+    Pagina principal
+@endsection
+
+@section('contenido')
+    <p>Contenido de la página principal</p>
+@endsection
