02f9b91e11f02af1eab624308a9328695aeef20e
Michael-Chacon <michaelchacon02@gmail.com>
2022-11-22 09:13:49 -0500

    Login y registro al 99%

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -10,5 +12,33 @@ class RegisterController extends Controller
     {
         return view('auth.register');
          
+    } 
+
+    public function store(Request $request)
+    {
+        $request->request->add(['username' => Str::slug($request->username)]);
+
+        $this->validate($request, [
+            'name' => 'required|min:5',
+            'username' => 'required|unique:users|min:3|max:20',
+            'email' => 'required|unique:users|email|max:60', 
+            'password' => 'required|confirmed|min:6',
+        ]);
+        
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+
+        /*auth()->attempt([
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+        */
+        auth()->attempt($request->only('email', 'password'));
+        
+        return redirect()->route('posts.index');
     }
 }
