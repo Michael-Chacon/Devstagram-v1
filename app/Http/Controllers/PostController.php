3773e12a93098f0b80b3ac8fcb4d3b6987141e61
Michael-Chacon <michaelchacon02@gmail.com>
2022-11-24 16:30:57 -0500

    Registrar y mostrar publicaciones

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -14,13 +15,49 @@ class PostController extends Controller
 
     public function index(User $user)
     {
+        // Utilisamos el Route Model Build para filtrar los posts por usuario
+        $posts = Post::where('user_id', $user->id)->get();
+
         return view('dashboard', [
-            'user' => $user
+            'user' => $user, 
+            'posts' => $posts
         ]);
     }
 
     public function create()
     {
         return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'titulo' => 'required|max:255',
+            'descripcion' => 'required',
+            'imagen' => 'required'
+        ]);
+
+        // Post::create([
+        //     'titulo' => $request->titulo,
+        //     'descripcion' => $request->descripcion,
+        //     'imagen' => $request->imagen,
+        //     'user_id' => auth()->user()->id
+        // ]);
+        // $post = new Post;
+        // $post->titulo = $request->titulo;
+        // $post->descripcion = $request->descripcion;
+        // $post->imagen = $request->imagen;
+        // $post->user_id = auth()->user()->id;
+        // $post->save();
+
+        // Guardar info utilizando la relación posts es el nombre de la relación 
+        $request->user()->posts()->create([
+            'titulo' => $request->titulo,
+            'descripcion' => $request->descripcion,
+            'imagen' => $request->imagen,
+            'user_id' => auth()->user()->id  
+        ]);
+
+        return redirect()->route('posts.index', auth()->user()->username);
     }
 }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -16,22 +16,28 @@
         </div>
 
         <div class="md:w-1/2 p-10  bg-white  rounded-lg shadow-lg mt-10 md:mt-0">
-            <form action="{{ route('register') }}" method="POST">
+            <form action="{{ route('posts.store') }}" method="POST">
                 @csrf
                 <div class="mb-5">
                     <label for="title" class="mb-2 block uppercase text-gray-500 font-bold">Title:</label>
-                    <input type="text" id="title" name="title" placeholder="Title of your publication" class="border p-3 w-full rounded-lg @error('title') border-red-500 @enderror" value="{{ old('title') }}">
-                    @error('title')
+                    <input type="text" id="title" name="titulo" placeholder="Title of your publication" class="border p-3 w-full rounded-lg @error('titulo') border-red-500 @enderror" value="{{ old('titulo') }}">
+                    @error('titulo')
                         <p class="bg-red-500 text-black my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
                     @enderror
                 </div>
                 <div class="mb-5">
                     <label for="description" class="mb-2 block uppercase text-gray-500 font-bold">Description:</label>
-                    <textarea type="text" id="description" name="description" placeholder="Description of your publication" class="border p-3 w-full rounded-lg @error('description') border-red-500 @enderror">{{ old('description') }}</textarea>
-                    @error('description')
+                    <textarea type="text" id="description" name="descripcion" placeholder="descripcion of your publication" class="border p-3 w-full rounded-lg @error('descripcion') border-red-500 @enderror">{{ old('descripcion') }}</textarea>
+                    @error('descripcion')
                         <p class="bg-red-500 text-black my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
                     @enderror
                 </div>
+
+                <input type="hidden" name="imagen" value="{{ old('imagen') }}">
+                @error('imagen')
+                    <p class="bg-red-500 text-black my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                @enderror
+
                 <input type="submit" value="Save publication" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
             </form>
         </div>
